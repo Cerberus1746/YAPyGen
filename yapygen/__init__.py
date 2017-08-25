efe71e4841617477e6e3f2ba1ce858bc27816095
cerberus1746 <cerberus1746@gmail.com>
2017-08-25 19:07:21 -0300

    Created new file for constants.

--- yapygen/__init__.py
+++ yapygen/__init__.py
@@ -1,24 +1,3 @@
-"""Teste!"""
+"""Main Module of the genetic Algorythm"""
 
-FITNESS_GENE_BASED = "fitnessGeneBased"
-FITNESS_GROUP_BASED = "fitnessGroupBased"
-FITNESS_SET = "fitnessSet"
-
-MUTATION_RECESSIVES = "mutationRecessives"
-
-FILTER_CUT_HALF = "filterCutHalf"
-
-ALL = "all"
-
-CROSSOVER_SIMPLE_SPLIT = "simpleCrossOverSplit"
-
-SPECIES_OPTIONS = [
-    "buffalo",
-    "dog",
-    "cat",
-    "merkat",
-    "dolphin",
-    "croc",
-    "alpaca",
-    "raven"
-]
+from yapygen.constants import *
---
+++ yapygen/constants.py
@@ -0,0 +1,22 @@
+FITNESS_GENE_BASED = "fitnessGeneBased"
+FITNESS_GROUP_BASED = "fitnessGroupBased"
+FITNESS_SET = "fitnessSet"
+
+MUTATION_RECESSIVES = "mutationRecessives"
+
+FILTER_CUT_HALF = "filterCutHalf"
+
+ALL = "all"
+
+CROSSOVER_SIMPLE_SPLIT = "simpleCrossOverSplit"
+
+SPECIES_OPTIONS = [
+    "buffalo",
+    "dog",
+    "cat",
+    "merkat",
+    "dolphin",
+    "croc",
+    "alpaca",
+    "raven"
+]
